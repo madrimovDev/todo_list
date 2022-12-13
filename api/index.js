cc82953092795b172c377e615f9351bfaa621d38
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2022-12-13 18:15:15 +0500

    backend

--- api/index.js
+++ api/index.js
@@ -4,18 +4,56 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.json()) //
 app.use(express.urlencoded({extended: true}))
+
 
 const todos = [
     {
         id: 1, 
         title: 'Todo Title',
         desc: 'Description'
+    },
+    {
+        id: 1, 
+        title: 'Todo Title 2',
+        desc: 'Description'
+    },
+    {
+        id: 1, 
+        title: 'Todo Title 3',
+        desc: 'Description'
+    },
+    {
+        id: 1, 
+        title: 'Todo Title 4',
+        desc: 'Description'
     }
 ]
 
 app.get('/todos', (req, res) => {
     res.json(todos)
+})
+
+app.post('/todos', (req, res)=>{
+
+    let { title, desc } = req.body
+
+    if(!title || !desc){
+        res.status(400).send({
+            msg: 'Title yoki description yo\'q'
+        })
+    }
+
+    const newTodo = {
+        id: Date.now(), 
+        title: req.body.title,
+        desc: req.body.desc
+    }
+    todos.push(newTodo)
+    res.send({
+        newTodo
+    })
 })
 
 app.listen(3030, () => {
--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-fetch('http://localhost:3030/todos')
-    .then(response => response.json())
-    .then(result => console.log(result))
+async function getTodos() {
+    const response = await fetch('http://localhost:3030/todos', {
+        method: 'GET'
+    })
+    const result = await response.json()
+    return result
+}
+
+function createTodo(todo) {
+    const li = document.createElement('li')
+    li.className = 'todos_item'
+    li.innerHTML = `<li class="todos__item">
+                        <span class="todos__span">${todo.title}</span>
+                        <span class="todos__span">${todo.desc}</span>
+                        <div class="todos__actions">
+                            <button class="todos__action"><i class='bx bxs-edit-alt'></i></button>
+                            <button class="todos__action"><i class='bx bxs-trash-alt'></i></button>
+                        </div>
+                    </li>`
+    document.querySelector('.todos').appendChild(li)
+}
+
+window.addEventListener('load', async () => {
+    console.log('loading')
+    let todos = await getTodos()
+    todos.forEach((item, index) => {
+        createTodo(item)
+    })
+})
