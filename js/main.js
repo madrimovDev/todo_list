e026fd69ab91e94703fd05ae1d3f751489b0831e
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2022-12-17 16:14:13 +0500

    s

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const todos = document.querySelector('.todos')
+const form = document.querySelector('.main-form')
+const input = document.querySelector('.form-input')
+const textarea = document.querySelector('.form-textarea')
 
-function createElement(tagName, className){
+function createElement(tagName, className) {
     const element = document.createElement(tagName)
     element.className = className
     return element
 }
 
-function createTodo(title, desc){
+function createTodo(title, desc) {
     const li = createElement('li', 'todos__item')
 
     const _title = createElement('span', 'todos__span')
@@ -33,20 +36,55 @@ function createTodo(title, desc){
     return li
 }
 
-async function getTodos(){
-    const response = await fetch('http://localhost:3030/todos')
+async function getTodos() {
+    const response = await fetch('http://localhost:3030/todos', {
+        method: 'GET'
+    })
     const result = await response.json()
     console.log(result)
     return result
 }
 
+async function setTodo(todo) {
+    const response = await fetch('http://localhost:3030/todos', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    const result = response.json()
+    console.log(result)
+    return result
+}
+
 window.addEventListener('DOMContentLoaded', async (event) => {
-   const todolist = await getTodos()
-   
-   todolist.forEach((item, index) => {
-    todos.appendChild(createTodo(item.title, item.desc))
-   })
+    const todolist = await getTodos()
+
+    todolist.forEach((item, index) => {
+        todos.appendChild(createTodo(item.title, item.desc))
+    })
 })
 
-// async 
+form.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const title = input.value
+    const desc = textarea.value
+    console.log('render')
+    if (!title || !desc) {
+        return
+    }
+
+    const result = await setTodo({
+        title,
+        desc
+    })
+
+    todos.appendChild(createTodo(result.newTodo.title, result.newTodo.desc))
+    
+    input.value = ''
+    textarea.value = ''
+})
+
+// async
 // sync
